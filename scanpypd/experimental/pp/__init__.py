3910e8da3665207951a032c031e608ea78d2b37a
Jacob Evarts <jacob.i.evarts@gmail.com>
2022-12-28 17:06:11 -0800

    Pandas boxplot is functioning

---
+++ scanpypd/experimental/pp/__init__.py
@@ -0,0 +1,8 @@
+from scanpypd.experimental.pp._normalization import (
+    normalize_pearson_residuals,
+    normalize_pearson_residuals_pca,
+)
+
+from scanpypd.experimental.pp._highly_variable_genes import highly_variable_genes
+
+from scanpypd.experimental.pp._recipes import recipe_pearson_residuals
